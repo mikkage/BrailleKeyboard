282f97dc48a89c34644a5fd2271afad5384fa223
Michael Parker <msparker@ucsc.edu>
2013-11-20 22:48:33 -0800

    Updated to latest version
    I still need to make the 2d array hold the appropriate characters.

--- braille_kb.ino
+++ braille_kb.ino
@@ -1,17 +1,20 @@
 
 //mux variables
-int input = 2;   // digital input to arduino from mux
-int strobe=5;   //digital outputs to control mux  
-int c=10;
-int b=9;
-int a=8;
+int input = 5;   // digital input to arduino from mux
+//int strobe=0;   //digital outputs to control mux  
+int c=4;
+int b=3;
+int a=2;
 
 bool storingInput = false;
 
 // to store the values from mux
-int val[] = {
-  0,0,0,0,0,0,0,0}; 
-int valsStored[] = {0,0,0,0,0,0,0,0}; 
+int val[] = {0,0,0,0,0,0,0,0}; 
+int valsStored[8] = {0,0,0,0,0,0,0,0}; 
+char characters[3][64];  //2d array to store characters.
+                         //[0][64] will contain lowercase letters
+                         //[1][64] will contain uppercase letters
+                         //[2][64] will contain numbers
 
 // digital values to control 8 inputs
 int c_bin[]={LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH};
@@ -29,10 +32,17 @@ void setup() {
 
   Serial.begin(9600);
   pinMode(input, INPUT);    
-  pinMode(strobe, OUTPUT);
+  //pinMode(strobe, OUTPUT);
   pinMode(c, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(a, OUTPUT);
+  for(int i = 0; i < 3; i++)
+  {
+     for(int j = 0; j < 64; j++)
+     {
+       characters[i][j] = NULL;
+     }
+  }
 }
 
 void loop() {
@@ -49,7 +59,7 @@ void loop() {
     digitalWrite(c,c_val);
 
     //strobe LOW to read
-    digitalWrite(strobe,LOW);
+    //digitalWrite(strobe,LOW);
 
     //read value
     val[entrada] = digitalRead(input);  // read input value
@@ -60,11 +70,17 @@ void loop() {
       allZero = false;
     }
     //strobe HIGH to avoid jitters
-    digitalWrite(strobe,HIGH);
+    //digitalWrite(strobe,HIGH);
   }
   if(allZero && storingInput)
   {
+      //check for backspace or space signals first.
+      
+      
      //use values in valsPressed to get the character to display. 
+     //int index = binToInt();       //convert six signals to an int to access arrays.
+     
+     //******letters******
      if(valsStored[0] && !valsStored[1] && !valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
        Serial.print('a');
      else if(valsStored[0] && valsStored[1] && !valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
@@ -117,7 +133,48 @@ void loop() {
        Serial.print('y');
      else if(valsStored[0] && !valsStored[1] && valsStored[2] && !valsStored[3] && valsStored[4] && valsStored[5])
        Serial.print('z');
-       else Serial.print("type a letter, asshole");
+       
+       
+       
+     //*****punctuation*****
+     else if(!valsStored[0] && valsStored[1] && !valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
+       Serial.print(',');
+     else if(!valsStored[0] && valsStored[1] && valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
+       Serial.print(';');
+     else if(!valsStored[0] && valsStored[1] && !valsStored[2] && !valsStored[3] && valsStored[4] && !valsStored[5])
+       Serial.print(':');
+     else if(!valsStored[0] && valsStored[1] && !valsStored[2] && !valsStored[3] && valsStored[4] && valsStored[5])
+       Serial.print('.');
+     else if(!valsStored[0] && valsStored[1] && valsStored[2] && !valsStored[3] && valsStored[4] && !valsStored[5])
+       Serial.print('!');
+     else if(!valsStored[0] && !valsStored[1] && !valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
+     {
+       //todo: choose to display "("or ")" depending on the last one displayed
+       //if(something)
+       //Serial.print('(');
+       //else
+       //Serial.print(')'); 
+     }
+     else if(!valsStored[0] && valsStored[1] && valsStored[2] && !valsStored[3] && !valsStored[4] && valsStored[5])
+       Serial.print('?');
+     else if(!valsStored[0] && !valsStored[1] && valsStored[2] && !valsStored[3] && valsStored[4] && !valsStored[5])
+       Serial.print('*');
+     else if(!valsStored[0] && !valsStored[1] && valsStored[2] && !valsStored[3] && valsStored[4] && valsStored[5])
+       Serial.print('"');
+     else if(!valsStored[0] && !valsStored[1] && valsStored[2] && !valsStored[3] && !valsStored[4] && !valsStored[5])
+       Serial.print("'");
+     else if(!valsStored[0] && !valsStored[1] && valsStored[2] && !valsStored[3] && !valsStored[4] && valsStored[5])
+       Serial.print('-');
+     
+     //*****Case and number
+     //uppercase
+     //lowercase
+     //numericals
+       
+     
+     //*****No case has matched, assume invalid input and don't print anything.
+       else 
+       Serial.print("not a valid character/not implemented yet");
        
      Serial.println();
      //Reset stored buttons
@@ -128,7 +185,7 @@ void loop() {
      }
   }
 
-/*
+
   //serial printing
   Serial.print('A');
   delay(100);
@@ -136,6 +193,19 @@ void loop() {
     Serial.print(val[contador]);
     delay(100);
   }
-  Serial.println();*/
+  Serial.println();
 
+}
+
+int binToInt(int valStore[8])
+{
+ int index = 0;
+ int multiplier = 1;
+ for(int i = 0; i <= 5; i++)
+ {
+   if(valStore[i] == 1)
+     index += multiplier;
+   multiplier *= 2;
+ }
+ return index; 
 }
