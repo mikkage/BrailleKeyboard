fdb86205a8d575f052ea9d34ae50b4178fb912d0
Michael Parker <msparker@ucsc.edu>
2013-11-21 18:07:06 -0800

    Set up character arrays

--- braille_kb.ino
+++ braille_kb.ino
@@ -36,6 +36,8 @@ void setup() {
   pinMode(c, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(a, OUTPUT);
+  
+  //Set up blank arrays
   for(int i = 0; i < 3; i++)
   {
      for(int j = 0; j < 64; j++)
@@ -43,6 +45,115 @@ void setup() {
        characters[i][j] = NULL;
      }
   }
+  //set up lowercase characters
+  characters[0][1] = 'a';
+  characters[0][3] = 'b';
+  characters[0][9] = 'c';
+  characters[0][25] = 'd';
+  characters[0][17] = 'e';
+  characters[0][11] = 'f';
+  characters[0][27] = 'g';
+  characters[0][19] = 'h';
+  characters[0][10] = 'i';
+  characters[0][26] = 'j';
+  characters[0][5] = 'k';
+  characters[0][7] = 'l';
+  characters[0][13] = 'm';
+  characters[0][29] = 'n';
+  characters[0][21] = 'o';
+  characters[0][15] = 'p';
+  characters[0][31] = 'q';
+  characters[0][23] = 'r';
+  characters[0][14] = 's';
+  characters[0][30] = 't';
+  characters[0][37] = 'u';
+  characters[0][39] = 'v';
+  characters[0][58] = 'w';
+  characters[0][45] = 'x';
+  characters[0][61] = 'y';
+  characters[0][53] = 'z';
+  
+  
+  //set up uppercase characters
+  characters[1][1] = 'A';
+  characters[1][3] = 'B';
+  characters[1][9] = 'C';
+  characters[1][25] = 'D';
+  characters[1][17] = 'E';
+  characters[1][11] = 'F';
+  characters[1][27] = 'G';
+  characters[1][19] = 'H';
+  characters[1][10] = 'I';
+  characters[1][26] = 'J';
+  characters[1][5] = 'K';
+  characters[1][7] = 'L';
+  characters[1][13] = 'M';
+  characters[1][29] = 'N';
+  characters[1][21] = 'O';
+  characters[1][15] = 'P';
+  characters[1][31] = 'Q';
+  characters[1][23] = 'R';
+  characters[1][14] = 'S';
+  characters[1][30] = 'T';
+  characters[1][37] = 'U';
+  characters[1][39] = 'V';
+  characters[1][58] = 'W';
+  characters[1][45] = 'X';
+  characters[1][61] = 'Y';
+  characters[1][53] = 'Z';
+  
+  //set up numbers
+  characters[2][1] = '1';
+  characters[2][3] = '2';
+  characters[2][9] = '3';
+  characters[2][25] = '4';
+  characters[2][17] = '5';
+  characters[2][11] = '6';
+  characters[2][27] = '7';
+  characters[2][19] = '8';
+  characters[2][10] = '9';
+  characters[2][26] = '0';
+  
+  //set up punctuation characters
+  characters[0][2] = ',';
+  characters[1][2] = ',';
+  characters[2][2] = ',';
+  characters[0][6] = ';';
+  characters[1][6] = ';';
+  characters[2][6] = ';';
+  characters[0][18] = ':';
+  characters[1][18] = ':';
+  characters[2][18] = ':';
+  characters[0][50] = '.';
+  characters[1][50] = '.';
+  characters[2][50] = '.';
+  characters[0][22] = '!';
+  characters[1][22] = '!';
+  characters[2][22] = '!';
+  characters[0][2] = ',';
+  characters[1][2] = ',';
+  characters[2][2] = ',';
+  
+  //todo: decide whether ( or ) is used.
+  characters[0][54] = '(';
+  characters[1][54] = '(';
+  characters[2][54] = '(';
+  
+  characters[0][38] = '?';
+  characters[1][38] = '?';
+  characters[2][38] = '?';
+  characters[0][20] = '*';
+  characters[1][20] = '*';
+  characters[2][20] = '*';
+  characters[0][52] = '"';
+  characters[1][52] = '"';
+  characters[2][52] = '"';
+  characters[0][4] = '\'';
+  characters[1][4] = '\'';
+  characters[2][4] = '\'';
+  characters[0][36] = '-';
+  characters[1][36] = '-';
+  characters[2][36] = '-';
 }
 
 void loop() {
